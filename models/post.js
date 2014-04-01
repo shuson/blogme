f26187853b95c9303093d20f3dd3aff8f5d35b40
shuson <wlgq@hotmail.com>
2014-04-01 13:16:50 +0800

    Update post.js
    add update method

--- models/post.js
+++ models/post.js
@@ -147,3 +147,52 @@ Post.removeById = function(id, callback){
 		});
 	});
 }
+
+//update post by id
+Post.update = function(id,post,callback){
+	var date = new Date();
+  
+	var time = {
+	    date: date,
+		year : date.getFullYear(),
+		month : date.getFullYear() + "-" + (date.getMonth() + 1),
+		day : date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
+		minute : date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + 
+		date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) 
+	}
+  
+  
+	mongodb.open(function (err, db) {
+		if (err) {
+		return callback(err);
+		}
+    
+		db.collection('posts', function (err, collection) {
+		if (err) {
+			mongodb.close();
+			return callback(err);
+		}
+
+		var query = {};
+		if(id){
+			query._id=new ObjectID(id);
+		}
+		var update={};
+		if(post){
+			update.time = time;
+			update.post = post;
+		}
+
+		collection.update(query,{$set:update}, {
+			safe: true,
+			multi:false
+		}, function (err,result) {
+			mongodb.close();
+			if (err) {
+				return callback(err);
+			}
+			callback(null);
+		});
+    });
+  });
+}
